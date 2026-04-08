7bc801dc64b459b4e772c6a4567814b2544d1a5e
dusi <luca.dusi@studenti.unipd.it>
2026-04-08 12:14:09 +0200

    piccole mod ad alcuni codici con commenti per facilitare lettura e modifiche successive

--- src/main/java/utils/DBConnection.java
+++ src/main/java/utils/DBConnection.java
@@ -6,10 +6,12 @@ import java.sql.SQLException;
 
 public class DBConnection {
 
+    //  to connect with database
     private static final String URL = "jdbc:postgresql://localhost:5432/lecture_notes";
     private static final String USER = "postgres";
     private static final String PASSWORD = "password";
 
+    // to obtain the connection
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
