be97a436df0e5a2f8b011843d5c5b72b24531356
TrifunovicMilos <milostrifunovic02@yahoo.it>
2026-06-02 16:32:26 +0200

    sidebar.js

---
+++ src/main/webapp/js/sidebar.js
@@ -0,0 +1,5 @@
+function toggleSidebar() {
+    document.getElementById("sidebar").classList.toggle("active");
+    document.getElementById("overlay").classList.toggle("active");
+    document.getElementById("menuBtn").classList.toggle("active");
+}
